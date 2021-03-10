3a1f8a61dac9b171b14bc52ce8ad7e1e341c89d9
HanSeungil2 <ouze@naver.com>
2021-03-10 18:10:30 +0900

    first commit

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    public float rotateSpeed = 10.0f;
+    public float zoomSpeed = 20.0f;
+
+    public GameObject myGameObj;
+
+    private Camera mainCamera;
+
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Zoom();
+        Rotate();
+    }
+
+    private void Zoom()
+    {
+        float distance = Input.GetAxis("Mouse ScrollWheel") * -1 * zoomSpeed;
+        if (distance != 0)
+        {
+            mainCamera.fieldOfView += distance;
+        }
+    }
+
+    private void Rotate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            mainCamera.transform.RotateAround(myGameObj.transform.position,
+                                        Vector3.up,
+                                        Input.GetAxis("Mouse X") * rotateSpeed);
+            mainCamera.transform.RotateAround(myGameObj.transform.position,
+                                            Vector3.right,
+                                            -Input.GetAxis("Mouse Y") * rotateSpeed);
+        }
+    }
+}
---
+++ Assets/Scripts/Import.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TriLib;
+
+public class Import : MonoBehaviour
+{
+    GameObject meshObj = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 파일 로드
+        using (var assetLoader = new AssetLoader())
+        {
+            var assetLoaderOptions = GetAssetLoaderOptions();
+            var wrapperGameObject = gameObject;
+            GameObject myGameObject = assetLoader.LoadFromFileWithTextures("D:\\Unity Hub\\project\\TestImport\\test.stl", assetLoaderOptions, wrapperGameObject);
+
+            //meshObj = FindMeshObject(myGameObject);
+
+            // 파일 이동/회전
+            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            transform.position = new Vector3(0, 0, 30);
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // 파일로더 옵션
+    private AssetLoaderOptions GetAssetLoaderOptions()
+    {
+        var assetLoaderOptions = AssetLoaderOptions.CreateInstance();
+        assetLoaderOptions.DontLoadCameras = false;
+        assetLoaderOptions.DontLoadLights = false;
+        assetLoaderOptions.UseOriginalPositionRotationAndScale = true;
+        assetLoaderOptions.DisableAlphaMaterials = true;
+        assetLoaderOptions.MaterialShadingMode = MaterialShadingMode.Roughness;
+        assetLoaderOptions.AddAssetUnloader = true;
+        assetLoaderOptions.AdvancedConfigs.Add(AssetAdvancedConfig.CreateConfig(AssetAdvancedPropertyClassNames.FBXImportDisableDiffuseFactor, true));
+        return assetLoaderOptions;
+    }
+
+    public GameObject FindMeshObject(GameObject obj)
+    {
+        int count = obj.transform.childCount;
+
+        if (count == 0) return null;
+
+        GameObject tObj = null;
+
+        for (int i = 0; i < obj.transform.childCount; i++)
+        {
+            tObj = FindMeshObject(obj.transform.GetChild(i).gameObject);
+
+            if (tObj != null)
+            {
+                return tObj;
+            }
+        }
+
+        return null;
+    }
+
+}
